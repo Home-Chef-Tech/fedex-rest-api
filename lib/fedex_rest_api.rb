f04f96ba56cc592d56022940b89f4d807957d15b
Elsie Koglin <23346890+elsiekoglin@users.noreply.github.com>
2024-03-14 13:57:33 -0500

    Base class inclusion

--- lib/fedex_rest_api.rb
+++ lib/fedex_rest_api.rb
@@ -1,6 +1,8 @@
 require "fedex_rest_api/version"
 
-module FedexRestApi
+class FedexRestApi
+  require "fedex_rest_api/base"
+
   class Error < StandardError; end
   # Your code goes here...
 end
---
+++ lib/fedex_rest_api/base.rb
@@ -0,0 +1,2 @@
+class FedexRestApi::Base
+end
