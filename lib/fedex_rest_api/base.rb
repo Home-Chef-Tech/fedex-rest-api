73b1bc2f8033ea06bf944c72b2abe5b328df9d3d
Elsie Koglin <23346890+elsiekoglin@users.noreply.github.com>
2024-03-18 11:38:36 -0500

    Token testing

--- lib/fedex_rest_api/base.rb
+++ lib/fedex_rest_api/base.rb
@@ -1,2 +1,15 @@
 class FedexRestApi::Base
+  # def initialize
+  #   HTTParty.get
+  # end
+
+  def self.fetch_token
+    HTTParty.post("https://apis-sandbox.fedex.com/oauth/token", {
+      body: {
+        client_id: "l71927121b3df94fb39be865f7794b35fa",
+        client_secret: "0293e021dba142a095ef91a7cdf84d1a",
+        grant_type: 'client_credentials'
+      }
+    })
+  end
 end
